72786d1dd758114a53edcfa15c7abd6e78e0faeb
Rafael_Rache <rafaelmarquesrache@gmail.com>
2020-08-13 18:46:32 -0300

    Add class relations

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -13,17 +13,29 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     CATEGORY = (
         ('Indoor', 'Indoor'),
         ('Out Door', 'Out Door'),
     )
-    
+
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     data_created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
@@ -33,7 +45,7 @@ class Order(models.Model):
         ('Delivered', 'Delivered')
     )
 
-    #customer =
-    #product =
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     data_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
