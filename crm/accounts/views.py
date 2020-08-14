17495e9043f127e82cb837129f18d2799e0a5c7a
Rafael_Rache <rafaelmarquesrache@gmail.com>
2020-08-14 00:07:07 -0300

    Add customer data

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -29,5 +29,14 @@ def products(request):
     return render(request, 'accounts/products.html', {'products': products})
 
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    orders_count = orders.count()
+
+    context = {
+        'customer': customer,
+        'orders': orders,
+        'orders_count': orders_count
+    }
+    return render(request, 'accounts/customer.html', context)
